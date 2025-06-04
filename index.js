b9478b066a24e91826750b2263cf187f1b245020
yflory <yann.flory@xwiki.com>
2025-06-04 16:09:36 +0200

    Add plugin config

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ const ACCOUNTS = {};
 const Express = require("express");
 const Path = require("node:path");
 const fs = require("fs");
+const config = require('./config/config.json');
 
 ACCOUNTS.addHttpEndpoints = (Env, app) => {
     let dir = Path.join(__dirname, 'client');
+    let json = Path.join(__dirname, 'config', 'plans.json');
+    app.get('/accounts/plans.json', (req, res) => {
+        res.sendFile(json);
+    });
     app.use('/accounts', Express.static(dir));
+};
+
+ACCOUNTS.getConfig = () => {
+    return config;
 };
 
 module.exports = {
